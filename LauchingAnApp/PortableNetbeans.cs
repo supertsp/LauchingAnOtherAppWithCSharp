d1d77e77441289189281ce8eb929149b8af67932
supertsp <tiago.souza.pedroso@gmail.com>
2019-06-05 08:15:21 -0300

    change project infos

--- LauchingAnApp/PortableNetbeans.cs
+++ LauchingAnApp/PortableNetbeans.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Program
+public class PortableNetbeans
 {
     public static void Main(string[] args)
     {
--- LauchingAnApp/Properties/AssemblyInfo.cs
+++ LauchingAnApp/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associada a um assembly.
-[assembly: AssemblyTitle("Apache Netbeans")]
+[assembly: AssemblyTitle("Portable Netbeans")]
 [assembly: AssemblyDescription("A Laucher for Apache Netbeans app")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The Apache Software Foundation")]
-[assembly: AssemblyProduct("Netbeans Portable")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("Copyright © 2017-2019 The Apache Software Foundation")]
+[assembly: AssemblyCompany("Tiago Penha Pedroso")]
+[assembly: AssemblyProduct("Portable Netbeans")]
+[assembly: AssemblyCopyright("GNU/GPL ver. 3")]
+[assembly: AssemblyTrademark("Portable Netbeans App")]
 [assembly: AssemblyCulture("")]
 
 // Definir ComVisible como false torna os tipos neste assembly invisíveis
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
