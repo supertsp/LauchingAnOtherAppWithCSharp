92dcc96f96aafd0317fd02ee7efb6338bd01ed0f
supertsp <tiago.souza.pedroso@gmail.com>
2019-06-08 17:01:18 -0300

    mudando xml a cada execução

--- LauchingAnApp/PortableNetbeans.cs
+++ LauchingAnApp/PortableNetbeans.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using static System.Console;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,21 @@ public class PortableNetbeans
 {
     public static void Main(string[] args)
     {
+
+        FileXML maven;
+
+        try
+        {            
+            string currentPath = Directory.GetCurrentDirectory();
+            string filePath = currentPath + @"\netbeans\java\maven\conf";
+            maven = new FileXML(filePath, "settings");
+            maven.GetElementsByTagName("localRepository")[0].InnerText = currentPath + @"\m2LocalRepository\";
+            maven.SaveAndWriteXMLFile();
+        }
+        catch (Exception) { }
+
         System.Diagnostics.Process.Start(@"netbeans\bin\netbeans64.exe");
+
+        ReadKey();
     }
 }
